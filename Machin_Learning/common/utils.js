ba119ca9fd63f17882498c0f05b2c0ae8a5f7343
Sidhesh Komarpant <komarpant.s@gmail.com>
2023-08-31 23:16:47 +0530

    data generator and viewer code

---
+++ Machin_Learning/common/utils.js
@@ -0,0 +1,41 @@
+const utils = {};
+
+utils.flaggedUsers = [1663882102141, 1663900040545, 1664485938220];
+
+utils.style = {
+    car:'gray',
+    fish:'red',
+    house:'yellow',
+    tree:'green',
+    bicycle:'cyan',
+    guiter:'blue',
+    pencil:'magenta',
+    clock:'lightgray',
+};
+
+utils.formatPercent = (n) => {
+    return (n * 100).toFixed(2) + "%";
+}
+
+utils.printProgress = (count, max) => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    const percent = utils.formatPercent(count/max);
+    process.stdout.write(count + "/" + max + "(" + percent + ")");
+}
+
+utils.groupBy = (objArray, key) => {
+    const groups = {};
+    for(let obj of objArray){
+        const val = obj[key];
+        if(groups[val] == null){
+            groups[val] = [];
+        }
+        groups[val].push(obj);
+    }
+    return groups;
+}
+
+if(typeof module !== "undefined"){
+    module.exports = utils;
+}
